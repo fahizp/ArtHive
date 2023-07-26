c5848728bf5bf928caad1b78e3c00ff4066cfdc9
fahizp <fahizp3@gmail.com>
2023-07-26 17:23:52 +0530

    feat:create constant file for data managemnt

---
+++ constant/index.ts
@@ -0,0 +1,96 @@
+export const NavLinks = [
+  { href: "/", key: "Vectors", text: "Vectors" },
+  { href: "/", key: "Photos", text: "Find Projects" },
+  { href: "/", key: "AI Images", text: "AI Images" },
+  { href: "/", key: "Icons", text: "Icons" },
+  { href: "/", key: "PSD", text: "PSD" },
+];
+
+export const categoryFilters = [
+  "Backgrounds",
+  "Textures",
+  "Patterns",
+  "Vectors",
+  "Illustrations",
+  "Drawings",
+  "Clip art",
+  "Silhouettes",
+  "Cartoons",
+  "Symbols and signs",
+  "Social media",
+  "business cards",
+  "Logos",
+];
+
+export const footerLinks = [
+  {
+    title: "CONTENT",
+    links: [
+      "Calendar of festivities",
+      "New assets",
+      "The most popular content",
+      "Search trends",
+      "Blog",
+    ],
+  },
+  {
+    title: "INFORMATION",
+    links: [
+      "Pricing",
+      "About us",
+      "Press room",
+      "Business",
+      "Sell your content",
+    ],
+  },
+  {
+    title: "LEGAL",
+    links: [
+      "Terms and conditions",
+      "License agreement",
+      "Privacy policy",
+      "Copyright information",
+      "Cookies policy",
+    ],
+  },
+  {
+    title: "SUPPORT",
+    links: [
+      "FAQ",
+      "Support",
+      "Terms of service",
+      "Privacy policy",
+      "Cookie policy",
+    ],
+  },
+  {
+    title: "Directories",
+    links: [
+      "Development jobs",
+      "Developers for hire",
+      "Freelance developers for hire",
+      "Tags",
+      "Places",
+    ],
+  },
+  {
+    title: "Development assets",
+    links: [
+      "Code Marketplace",
+      "GitHub Marketplace",
+      "NPM Registry",
+      "Packagephobia",
+    ],
+  },
+  {
+    title: "Development Resources",
+    links: [
+      "Freelancing",
+      "Development Hiring",
+      "Development Portfolio",
+      "Development Education",
+      "Creative Process",
+      "Development Industry Trends",
+    ],
+  },
+];
