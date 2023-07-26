f1ccef4ea6ca1c0c0fc486b95e0438a3f065db9d
fahizp <fahizp3@gmail.com>
2023-07-26 17:27:40 +0530

    change the color of the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         'black-100': '#252525',
         'primary-purple': '#9747FF',
         'gray-50': '#D9D9D9',
+        'footer-black': '#1d262d'
       },
       boxShadow: {
         menu: '0px 159px 95px rgba(13,12,34,0.01), 0px 71px 71px rgba(13,12,34,0.02), 0px 18px 39px rgba(13,12,34,0.02), 0px 0px 0px rgba(13,12,34,0.02)',
