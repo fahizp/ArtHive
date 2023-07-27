585a6488b9dce46a90056a214495e7cd54f96f09
fahizp <fahizp3@gmail.com>
2023-07-27 12:34:40 +0530

    feat: add the grafbase basic config with the sdk

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,25 @@
+import { g, auth, config } from "@grafbase/sdk";
+
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+});
+
+const Project = g.model("Project", {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url().optional(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+});
+
+export default config({
+  schema: g,
+});
