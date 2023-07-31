b59a813cbe116ef02d89751013f4af2a5581f2cc
fahizp <fahizp3@gmail.com>
2023-07-31 15:47:23 +0530

    feat: create new graphQL client to connect the grapfbase act as the db create the 2 fuctions getUser, and the create user

---
+++ lib/actions.ts
@@ -0,0 +1,42 @@
+import { GraphQLClient } from "graphql-request";
+import { createUserMutation, getUserQuery } from "@/graphql";
+
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "http://127.0.0.1:4000/graphql";
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+    return await client.request(query, variables);
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getUser = (email: string) => {
+  client.setHeader("x-api-key", apiKey);
+  return makeGraphQLRequest(getUserQuery, { email });
+};
+
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+  client.setHeader("x-api-key", apiKey);
+
+  const variables = {
+    input: {
+      name: name,
+      email: email,
+      avatarUrl: avatarUrl,
+    },
+  };
+
+  return makeGraphQLRequest(createUserMutation, variables);
+};
