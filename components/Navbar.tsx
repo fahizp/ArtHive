18e06530d308a61161159cb1e8becbe816dc538c
fahizp <fahizp3@gmail.com>
2023-07-31 15:43:23 +0530

    feat:add the new profilemenu components for the avoide the navbar to make the client side componet

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import { NavLinks } from "@/constant";
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "@/components/AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = true;
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -20,9 +22,9 @@ const Navbar = () => {
         </ul>
       </div>
       <div className='flexCenter gap-4'>
-      {session ? (
+      {session ?.user ? (
           <>
-            <Link href="/create-project">Profile</Link>
+            <ProfileMenu session={session} />
 
             <Link href="/create-project">Share Work</Link>
           </>
