b22944a05c04bfd3a50c81ca80cb1765b38a97ae
fahizp <fahizp3@gmail.com>
2023-08-06 17:34:43 +0530

    feat: create new route the folder stracuer for the create post

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -11,7 +11,7 @@ const CreateProject = async () => {
 
   return (
     <Modal>
-      <h3 className="modal-head-text">Create a New Project</h3>
+      <h3 className="modal-head-text create_gradient">Create a New Project</h3>
 
       <ProjectForm type="create" session={session} />
     </Modal>
