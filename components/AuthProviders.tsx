73ec74a224013b5e5e054e941f0312feaa3ea90b
fahizp <fahizp3@gmail.com>
2023-08-06 17:37:44 +0530

    fix: change the provider button into the new dyanamic button

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,40 +1,44 @@
-"use client";
+"use client"
 
-import { getProviders, signIn } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import { getProviders, signIn } from 'next-auth/react';
+import React, { useEffect, useState } from 'react'
+
+import Button from './Button';
 
 type Provider = {
-  id: string;
-  name: string;
-  type: string;
-  signinUrl: string;
-  callbackUrl: string;
-  signinUrlParams?: Record<string, string> | undefined;
-};
-type Providers = Record<string, Provider>;
-const AuthProviders = () => {
-  const [providers, setProviders] = useState<Providers | null>(null);
-
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | undefined;
+  };
   
-  useEffect(() => {
-    const fetchProviders = async () => {
-        const res = await getProviders();
+  type Providers = Record<string, Provider>;
 
-        setProviders(res);
+
+const AuthProviders = () => {
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+    
+            setProviders(res);
+        }
+
+        fetchProviders();
+    }, []);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <Button key={i} bgColor={"bg-nav-button"} title='Sign In' handleClick={() => signIn(provider?.id)} />
+                ))}
+            </div>
+        )
     }
+}
 
-    fetchProviders();
-}, []);
-
-  if (providers) {
-    return (
-      <div>
-        {Object.values(providers).map((provider: Provider, i) => (
-          <button key={i}  onClick={() => signIn(provider?.id)}>{provider.id}</button>
-        ))}
-      </div>
-    );
-  }
-};
-
-export default AuthProviders;
+export default AuthProviders
